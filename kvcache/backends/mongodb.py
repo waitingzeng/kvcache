7bd26020e1c1889f286ac8b4449173a566738e75
ttwait <ttwait@gmail.com>
2013-06-19 14:28:30 +0800

    fix bug

---
+++ kvcache/backends/mongodb.py
@@ -0,0 +1,131 @@
+import time
+from .db import BaseDatabaseCache
+from django.db import connections, router
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+import pymongo
+import bson
+
+class MongoDBCache(BaseDatabaseCache):
+    def validate_key(self, key):
+        if '.' in key or '$' in key:
+            raise ValueError("Cache keys must not contain '.' or '$' "
+                             "if using MongoDB cache backend")
+        super(MongoDBCache, self).validate_key(key)
+
+    def get(self, key, default=None, version=None, raw=False, raw_key=False):
+        if not raw_key:
+            key = self.make_key(key, version=version)
+        self.validate_key(key)
+
+        collection = self._collection_for_read()
+        document = collection.find_one({'_id' : key})
+
+        if document is None:
+            return default
+
+        if document['e'] < time.time():
+            # outdated document, delete it and pretend it doesn't exist
+            collection.remove({'_id' : key})
+            return default
+
+        if raw:
+            return document
+
+        pickled_obj = document.get('p')
+        if pickled_obj is not None:
+            return pickle.loads(pickled_obj)
+        else:
+            return document['v']
+
+    def has_key(self, key, version=None):
+        return self.get(key, version=version, raw=True) is not None
+
+    def set(self, key, value, timeout=None, version=None):
+        self._base_set(key, value, timeout, version, force_set=True)
+
+    def add(self, key, value, timeout=None, version=None):
+        return self._base_set(key, value, timeout, version, force_set=False)
+
+    def _base_set(self, key, value, timeout, version, force_set=False):
+        collection = self._collection_for_write()
+
+        if collection.count() > self._max_entries:
+            self._cull(collection)
+
+        if not force_set and self.has_key(key, version):
+            # do not overwrite existing, non-expired entries.
+            return False
+
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+        now = time.time()
+        expires = now + (timeout or self.default_timeout)
+        new_document = {'_id' : key, 'v' : value, 'e' : expires}
+
+        try:
+            collection.save(new_document)
+        except bson.errors.InvalidDocument:
+            # value can't be serialized to BSON, fall back to pickle.
+
+            # TODO: Suppress PyMongo warning here by writing a PyMongo patch
+            # that allows BSON to be passed as document to .save
+            pickle_blob = pickle.dumps(new_document.pop('v'), protocol=2)
+            new_document['p'] = bson.binary.Binary(pickle_blob)
+            collection.save(new_document)
+
+        return True
+
+    def incr(self, key, delta=1, version=None):
+        key = self.make_key(key, version=version)
+        collection = self._collection_for_write()
+        update_args = [{'_id': key}, {'$inc': {'v': delta}}]
+
+        if hasattr(pymongo.collection.Collection, 'find_and_modify'):
+            # XXX change this to tuple/int comparison once
+            # there's a pymongo.version tuple equivalent
+            new_document = collection.find_and_modify(*update_args, new=True, fields=['v'])
+            if new_document is None:
+                raise ValueError("Key %r not found" % key)
+            return new_document['v']
+        else:
+            document = self.get(key, version=version, raw=True, raw_key=True)
+            if document is None:
+                raise ValueError("Key %r not found" % key)
+            new_value = document['v'] + delta
+            collection.update(*update_args)
+            return new_value
+
+    def delete(self, key, version=None):
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+        self._collection_for_write().remove({'_id' : key})
+
+    def clear(self):
+        self._collection_for_write().drop()
+
+    def _cull(self, collection):
+        if self._cull_frequency == 0:
+            self.clear()
+            return
+
+        collection.remove({'e' : {'$lt' : time.time()}})
+        # remove all expired entries
+        count = collection.count()
+        if count > self._max_entries:
+            # still too much entries left
+            cut = collection.find({}, {'e' : 1}) \
+                            .sort('e', pymongo.ASCENDING) \
+                            .skip(count / self._cull_frequency).limit(1)[0]
+            collection.remove({'e' : {'$lt' : cut['e']}}, safe=True)
+
+    def _collection_for_read(self):
+        db = router.db_for_read(self.cache_model_class)
+        return connections[db].database[self._table]
+
+    def _collection_for_write(self):
+        db = router.db_for_write(self.cache_model_class)
+        return connections[db].database[self._table]
---
+++ kvcache/backends/s3.py
@@ -0,0 +1,204 @@
+"Amazon S3 cache backend for Django"
+
+# Copyright (c) 2012, Alexander Todorov <atodorov@nospam.otb.bg>
+#
+# Taken directly from django.core.cache.backends.filebased.FileBasedCache
+# and adapted for S3.
+
+import time
+import hashlib
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+from storages.backends import s3boto
+from django.core.files.base import ContentFile
+from .base import BaseCache
+
+class AmazonS3Cache(BaseCache):
+    def __init__(self, location, params):
+        """
+            location is not used but otherwise Django crashes.
+        """
+
+        BaseCache.__init__(self, params)
+
+        # Amazon and boto has a maximum limit of 1000 for get_all_keys(). See:
+        # http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGET.html
+        # This implementation of the GET operation returns some or all (up to 1000) of the objects in a bucket....
+
+        if self._max_entries > 1000:
+            self._max_entries = 1000
+
+        self._options = params.get('OPTIONS', {})
+
+        # backward compatible syntax for s3cache users before v1.2 for easy upgrades
+        # in v1.2 we update to latest django-storages 1.1.8 which changes variable names
+        # in non-backward compatible fashion
+        self._options['ACCESS_KEY'] = self._options.get('ACCESS_KEY_ID', None)
+        self._options['SECRET_KEY'] = self._options.get('SECRET_ACCESS_KEY', None)
+        self._options['BUCKET_NAME'] = self._options.get('STORAGE_BUCKET_NAME', None)
+
+        # we use S3 compatible varibale names while django-storages doesn't
+        _BUCKET_NAME = self._options.get('BUCKET_NAME', None)
+        _DEFAULT_ACL = self._options.get('DEFAULT_ACL', 'private')
+        _BUCKET_ACL  = self._options.get('BUCKET_ACL', _DEFAULT_ACL)
+        # in case it was not specified in OPTIONS default to 'private'
+        self._options['BUCKET_ACL'] = _BUCKET_ACL
+
+        # sanitize location by removing leading and traling slashes
+        self._LOCATION = self._options.get('LOCATION', self._options.get('location', ''))
+
+        while self._LOCATION.startswith('/'):
+            self._LOCATION = self._LOCATION[1:]
+
+        while self._LOCATION.endswith('/'):
+            self._LOCATION = self._LOCATION[:-1]
+
+        # overwrite value specified by user
+        self._options['LOCATION'] = self._LOCATION
+
+        # S3BotoStorage wants lower case names
+        for name, value in self._options.items():
+            if value is not None: # skip None values
+                self._options[name.lower()] = value
+
+        self._storage = s3boto.S3BotoStorage(
+                                    acl=_DEFAULT_ACL,
+                                    bucket=_BUCKET_NAME,
+                                    **self._options
+                                )
+
+
+    def add(self, key, value, timeout=None, version=None):
+        if self.has_key(key, version=version):
+            return False
+
+        self.set(key, value, timeout, version=version)
+        return True
+
+    def get(self, key, default=None, version=None):
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+
+        fname = self._key_to_file(key)
+        try:
+            f = self._storage.open(fname, 'rb')
+            try:
+                exp = pickle.load(f)
+                now = time.time()
+                if exp < now:
+                    self._delete(fname)
+                else:
+                    return pickle.load(f)
+            finally:
+                f.close()
+        except (IOError, OSError, EOFError, pickle.PickleError):
+            pass
+        return default
+
+    def set(self, key, value, timeout=None, version=None):
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+
+        fname = self._key_to_file(key)
+
+        if timeout is None:
+            timeout = self.default_timeout
+
+        self._cull()
+
+        try:
+            now = time.time()
+            content = pickle.dumps(now + timeout, pickle.HIGHEST_PROTOCOL)
+            content += pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
+            self._storage.save(fname, ContentFile(content))
+        except (IOError, OSError, EOFError, pickle.PickleError):
+            pass
+
+    def delete(self, key, version=None):
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+        try:
+            self._delete(self._key_to_file(key))
+        except (IOError, OSError):
+            pass
+
+    def _delete(self, fname):
+        self._storage.delete(fname)
+
+    def has_key(self, key, version=None):
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+        fname = self._key_to_file(key)
+        try:
+            f = self._storage.open(fname, 'rb')
+            try:
+                exp = pickle.load(f)
+                now = time.time()
+                if exp < now:
+                    self._delete(fname)
+                    return False
+                else:
+                    return True
+            finally:
+                f.close()
+        except (IOError, OSError, EOFError, pickle.PickleError):
+            return False
+
+    def _cull(self):
+
+        if not self._max_entries:
+            return
+        elif int(self._num_entries) < self._max_entries:
+            return
+
+        try:
+            keylist = self._storage.bucket.get_all_keys(prefix=self._LOCATION)
+        except:
+            return
+
+        if self._cull_frequency == 0:
+            doomed = keylist
+        else:
+            doomed = [k for (i, k) in enumerate(keylist) if i % self._cull_frequency == 0]
+
+        try:
+            self._storage.bucket.delete_keys(doomed, quiet=True)
+        except:
+            pass
+
+    def _key_to_file(self, key):
+        """
+            All files go into a single flat directory because it's not easier
+            to search/delete empty directories in _delete().
+
+            Plus Amazon S3 doesn't seem to have a problem with many files into one directory.
+
+            NB: measuring sha1() with timeit shows it is a bit faster compared to md5()
+            http://stackoverflow.com/questions/2241013/is-there-a-significant-overhead-by-using-different-versions-of-sha-hashing-hash
+
+            UPDATE: this is wrong, md5() is still faster, see:
+            http://atodorov.org/blog/2013/02/05/performance-test-md5-sha1-sha256-sha512/
+        """
+        return hashlib.sha1(key).hexdigest()
+
+    def _get_num_entries(self):
+        """
+            There seems to be an artificial limit of 1000
+        """
+        return len(self._storage.bucket.get_all_keys(prefix=self._LOCATION))
+    _num_entries = property(_get_num_entries)
+
+    def clear(self):
+        try:
+            all_keys = self._storage.bucket.get_all_keys(prefix=self._LOCATION)
+            self._storage.bucket.delete_keys(all_keys, quiet=True)
+        except:
+            pass
+
+# For backwards compatibility
+class CacheClass(AmazonS3Cache):
+    pass
